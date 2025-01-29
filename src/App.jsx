1052cb7aa62d4a2fb4164f1e3a2d3a26cda4af47
- Manraj Singh <manrajmarok41@gmail.com>
2025-01-29 08:32:04 -0500

    Setting up Project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import {Canvas} from '@react-three/fiber'
+import Experience from './components/Experience'; 
+
+function App() {
+
+  return (
+    <>
+      <Canvas>
+        <Experience />
+      </Canvas>
+    </>
+  )
+}
+
+export default App
